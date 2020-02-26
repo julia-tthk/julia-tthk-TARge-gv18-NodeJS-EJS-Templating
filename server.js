002d2642d184464ee2d96eb2fae02d4399763336
Julia Aasmaa <my_e_10@hotmail.com>
2020-02-26 15:35:10 +0200

    V1

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname, 'views', 'shop.html'));
+})
+
+app.use(function(req, res, next){
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    console.log(path.join(__dirname, 'views', '404.html'));
+});
+
+
+app.listen(5000, ()=> {
+    console.log("Server is running on Port 3000.");
+})
